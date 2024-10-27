d685fcfa79e2c246e722faa849499dace560edf5
chrispwd <chrispwd@protonmail.com>
2024-10-26 23:46:19 -0600

    style: commitmono font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "ComicShannsMono Nerd Font:style=Regular:pixelsize=20";
+static char *font = "CommitMono Nerd Font Mono:style=Regular:pixelsize=18";
 static int borderpx = 2;
 
 /*
