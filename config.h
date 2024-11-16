892ea61d7de49f0bfd83e34e8de821a870cf6236
chrispwd <chrispwd@protonmail.com>
2024-11-15 23:12:52 -0700

    fix: default to fontconfig for monospace font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "CommitMono Nerd Font Mono:style=Regular:pixelsize=18";
+static char *font = "monospace:size=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
