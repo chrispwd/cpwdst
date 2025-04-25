3aaf3509a0ebf2c60ab9da92739d420831c4b7c7
chrispwd <chrispwd@protonmail.com>
2025-04-25 02:08:53 -0600

    refactor: light theme

--- config.def.h
+++ config.def.h
@@ -95,31 +95,30 @@ unsigned int tabspaces = 4;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-  /* sanity-tomorrow-eighties */
-    /* 8 normal colors */
-    "#393939", /* black   */
-    "#a54242", /* red     */
-    "#8c9440", /* green   */
-    "#de935f", /* yellow  */
-    "#5f819d", /* blue    */
-    "#85678f", /* magenta */
-    "#5e8d87", /* cyan    */
-    "#888888", /* white   */
+  /* aquarium-light */
+ /* 8 normal colors */
+  [0] = "#313449", /* black   */
+  [1] = "#C34864", /* red     */
+  [2] = "#7D9685", /* green   */
+  [3] = "#DE956F", /* yellow  */
+  [4] = "#6A8CBC", /* blue    */
+  [5] = "#8787BF", /* magenta */
+  [6] = "#829FB0", /* cyan    */
+  [7] = "#D5D4E0", /* white   */
 
-    /* 8 bright colors */
-    "#515151", /* black   */
-    "#cc6666", /* red     */
-    "#b5bd68", /* green   */
-    "#f0c674", /* yellow  */
-    "#81a2be", /* blue    */
-    "#b294bb", /* magenta */
-    "#8abeb7", /* cyan    */
-    "#acafad", /* white   */
+  /* 8 bright colors */
+  [8]  = "#414560", /* black   */
+  [9]  = "#B7435E", /* red     */
+  [10] = "#7e8f80", /* green   */
+  [11] = "#D88B72", /* yellow  */
+  [12] = "#6E7EBF", /* blue    */
+  [13] = "#7170C2", /* magenta */
+  [14] = "#728A9A", /* cyan    */
+  [15] = "#CCCBD9", /* white   */
 
-	[255] = 0,
   /* special colors */
-  "#2d2d2d", /* background */
-  "#acafad", /* foreground */
+  [256] = "#E6E6F1", /* background */
+  [257] = "#708190", /* foreground */
 };
 
 /*
@@ -128,7 +127,7 @@ static const char *colorname[] = {
  */
 unsigned int defaultfg = 257;
 unsigned int defaultbg = 256;
-unsigned int defaultcs = 12;
+unsigned int defaultcs = 257;
 static unsigned int defaultrcs = 15;
 
 /*
