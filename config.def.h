77486e542dd0230ad84a4613be6850fad261e6d4
chrispwd <chrispwd@protonmail.com>
2025-06-10 21:28:09 -0600

    fix: bad static declaration

--- config.def.h
+++ config.def.h
@@ -125,9 +125,9 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor
  */
-static unsigned int defaultfg = 257;
-static unsigned int defaultbg = 256;
-static unsigned int defaultcs = 257;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+unsigned int defaultcs = 257;
 static unsigned int defaultrcs = 257;
 
 /*
