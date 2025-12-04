5305b84f6f0d7cd986a1330a60d2c3e1979195d9
chrispwd <chrispwd@protonmail.com>
2025-12-03 23:27:18 -0700

    feat: twilight theme

--- config.def.h
+++ config.def.h
@@ -97,28 +97,28 @@ unsigned int tabspaces = 4;
 static const char *colorname[] = {
 
   /* 8 normal colors */
-  [0] = "#073642", /* black   */
-  [1] = "#dc322f", /* red     */
-  [2] = "#859900", /* green   */
-  [3] = "#b58900", /* yellow  */
-  [4] = "#268bd2", /* blue    */
-  [5] = "#d33682", /* magenta */
-  [6] = "#2aa198", /* cyan    */
-  [7] = "#eee8d5", /* white   */
+  [0] = "#1e1e1e", /* black   */
+  [1] = "#cf6a4c", /* red     */
+  [2] = "#8f9d6a", /* green   */
+  [3] = "#d4cb81", /* yellow  */
+  [4] = "#7587a6", /* blue    */
+  [5] = "#dc8cc3", /* magenta */
+  [6] = "#9fb3c7", /* cyan    */
+  [7] = "#a7a7a7", /* white   */
 
   /* 8 bright colors */
-  [8]  = "#002b36", /* black   */
-  [9]  = "#cb4b16", /* red     */
-  [10] = "#586e75", /* green   */
-  [11] = "#657b83", /* yellow  */
-  [12] = "#839496", /* blue    */
-  [13] = "#6c71c4", /* magenta */
-  [14] = "#93a1a1", /* cyan    */
-  [15] = "#fdf6e3", /* white   */
+  [8]  = "#323537", /* black   */
+  [9]  = "#cda869", /* red     */
+  [10] = "#5f5a60", /* green   */
+  [11] = "#838184", /* yellow  */
+  [12] = "#919191", /* blue    */
+  [13] = "#a7a7a7", /* magenta */
+  [14] = "#9b703f", /* cyan    */
+  [15] = "#c3c3c3", /* white   */
 
   /* special colors */
-  [256] = "#002b36", /* background */
-  [257] = "#839496", /* foreground */
+  [256] = "#1e1e1e", /* background */
+  [257] = "#919191", /* foreground */
 };
 
 /*
@@ -127,8 +127,9 @@ static const char *colorname[] = {
  */
 unsigned int defaultfg = 257;
 unsigned int defaultbg = 256;
-unsigned int defaultcs = 257;
+unsigned int defaultcs = 6;
 unsigned int defaultrcs = 8;
+
 
 /*
  * Colors used, when the specific fg == defaultfg. So in reverse mode this
